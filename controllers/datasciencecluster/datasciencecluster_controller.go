997acea518e86ffc7be902c5804e223ecaacf8ed
Wen Zhou <wenzhou@redhat.com>
2023-11-20 19:11:03 +0100

    cherrypick: upstream 748 fix no reconcile when no error return
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- controllers/datasciencecluster/datasciencecluster_controller.go
+++ controllers/datasciencecluster/datasciencecluster_controller.go
@@ -94,9 +94,11 @@ func (r *DataScienceClusterReconciler) Reconcile(ctx context.Context, req ctrl.R
 		// Owned objects are automatically garbage collected. For additional cleanup logic use operatorUninstall function.
 		// Return and don't requeue
 		if upgrade.HasDeleteConfigMap(r.Client) {
-			return reconcile.Result{}, fmt.Errorf("error while operator uninstall: %v",
-				upgrade.OperatorUninstall(r.Client, r.RestConfig))
+			if uninstallErr := upgrade.OperatorUninstall(r.Client, r.RestConfig); uninstallErr != nil {
+				return ctrl.Result{}, fmt.Errorf("error while operator uninstall: %v", uninstallErr)
+			}
 		}
+
 		return ctrl.Result{}, nil
 	}
 
