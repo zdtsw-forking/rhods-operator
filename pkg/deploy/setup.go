4616c4775f796c76d9dc47555657e57016a8ae29
Wen Zhou <wenzhou@redhat.com>
2024-01-19 08:32:10 +0100

    fix: couple of changes (#164)
    * fix: couple of changes
    
    - change from 2.5 missing on 2.6
    - version/branch from components
    - generated bundle with rbac
    - linter
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * [cherry-pick]: odh https://github.com/opendatahub-io/opendatahub-operator/pull/809
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- pkg/deploy/setup.go
+++ pkg/deploy/setup.go
@@ -13,7 +13,7 @@ import (
 const (
 	// ManagedRhods defines expected addon catalogsource.
 	ManagedRhods Platform = "addon-managed-odh-catalog"
-	// SelfManagedRhods defines display name in csv
+	// SelfManagedRhods defines display name in csv.
 	SelfManagedRhods Platform = "Red Hat OpenShift AI"
 	// OpenDataHub defines display name in csv.
 	OpenDataHub Platform = "Open Data Hub Operator"
