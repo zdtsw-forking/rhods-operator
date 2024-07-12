b14feb104f6b7b88c3757c7fae3b84b53114c251
Wen Zhou <wenzhou@redhat.com>
2024-07-12 17:11:36 +0200

    update: improve getting platform value from operator (#303)
    * fix/cherry-pick: performance enhancement
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- pkg/deploy/setup.go
+++ pkg/deploy/setup.go
@@ -2,11 +2,8 @@ package deploy
 
 import (
 	"context"
-	"strings"
 
 	ofapi "github.com/operator-framework/api/pkg/operators/v1alpha1"
-	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -23,57 +20,35 @@ const (
 
 type Platform string
 
-// isSelfManaged checks presence of ClusterServiceVersions:
-// when CSV displayname contains OpenDataHub, return 'OpenDataHub,nil' => high priority
-// when CSV displayname contains SelfManagedRhods, return 'SelfManagedRhods,nil'
-// when in dev mode and  could not find CSV (deploy by olm), return "", nil
-// otherwise return "",err.
+// isSelfManaged detects if it is Self Managed Rhods or OpenDataHub.
 func isSelfManaged(cli client.Client) (Platform, error) {
-	clusterCsvs := &ofapi.ClusterServiceVersionList{}
-	err := cli.List(context.TODO(), clusterCsvs)
-	if err != nil {
-		return "", err
-	} else { //nolint:golint,revive // Readability on else
-		for _, csv := range clusterCsvs.Items {
-			if strings.Contains(csv.Spec.DisplayName, string(OpenDataHub)) {
-				return OpenDataHub, nil
-			}
-			if strings.Contains(csv.Spec.DisplayName, string(SelfManagedRhods)) {
-				return SelfManagedRhods, nil
-			}
+	variants := map[string]Platform{
+		"opendatahub-operator": OpenDataHub,
+		"rhods-operator":       SelfManagedRhods,
+	}
+
+	for k, v := range variants {
+		exists, err := OperatorExists(cli, k)
+		if err != nil {
+			return Unknown, err
+		}
+		if exists {
+			return v, nil
 		}
 	}
+
 	return Unknown, nil
 }
 
-// isManagedRHODS checks if CRD add-on exists and contains string ManagedRhods.
+// isManagedRHODS checks if catsrc CR add-on exists ManagedRhods.
 func isManagedRHODS(cli client.Client) (Platform, error) {
-	catalogSourceCRD := &apiextv1.CustomResourceDefinition{}
+	catalogSource := &ofapi.CatalogSource{}
 
-	err := cli.Get(context.TODO(), client.ObjectKey{Name: "catalogsources.operators.coreos.com"}, catalogSourceCRD)
+	err := cli.Get(context.TODO(), client.ObjectKey{Name: "addon-managed-odh-catalog", Namespace: "openshift-marketplace"}, catalogSource)
 	if err != nil {
-		if apierrs.IsNotFound(err) {
-			return "", nil
-		}
-		return "", err
+		return Unknown, client.IgnoreNotFound(err)
 	}
-	expectedCatlogSource := &ofapi.CatalogSourceList{}
-	err = cli.List(context.TODO(), expectedCatlogSource)
-	if err != nil {
-		if apierrs.IsNotFound(err) {
-			return Unknown, nil
-		}
-		return Unknown, err
-	}
-	if len(expectedCatlogSource.Items) > 0 {
-		for _, cs := range expectedCatlogSource.Items {
-			if cs.Name == string(ManagedRhods) {
-				return ManagedRhods, nil
-			}
-		}
-	}
-
-	return "", nil
+	return ManagedRhods, nil
 }
 
 func GetPlatform(cli client.Client) (Platform, error) {
